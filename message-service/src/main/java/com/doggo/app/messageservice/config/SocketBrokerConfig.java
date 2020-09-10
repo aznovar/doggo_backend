ec31c52249da0078562ac96a6aa01df1d997a8fa
aznovar <kkrieger5@gmail.com>
2020-09-10 21:11:04 +0300

    Добавил зависимости для подключения к STOMP порту Добавил константы для указания топиков и очередей Добавил возможность подключения к внешнему брокеру сообщений

--- message-service/src/main/java/com/doggo/app/messageservice/config/SocketBrokerConfig.java
+++ message-service/src/main/java/com/doggo/app/messageservice/config/SocketBrokerConfig.java
@@ -6,19 +6,31 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import static com.doggo.app.messageservice.consts.NameOfEndpointsForBrokerPath.*;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class SocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/secured/user/queue/specific-user");
         registry.setApplicationDestinationPrefixes("/spring-mvc-socket");
         registry.setUserDestinationPrefix("/secured/user");
+        //registry.enableStompBrokerRelay(CHAT_WITH_SPECIFIC_USER, CHAT_IN_COMMON_CHANNEL);
+        registry.enableStompBrokerRelay(CHAT_WITH_SPECIFIC_USER, CHAT_IN_COMMON_CHANNEL)
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("secured/room").withSockJS();
+        registry.addEndpoint(SECURED_CHAT)
+                .setAllowedOrigins()
+                .withSockJS();
+        registry.addEndpoint(SECURED_CHAT_ROOM)
+                .setAllowedOrigins()
+                .withSockJS();
     }
 }
--- message-service/src/main/java/com/doggo/app/messageservice/consts/NameOfEndpointsForBrokerPath.java
+++ message-service/src/main/java/com/doggo/app/messageservice/consts/NameOfEndpointsForBrokerPath.java
@@ -5,5 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class NameOfEndpointsForBrokerPath {
 
+    public final static String CHAT_WITH_SPECIFIC_USER = "/chat/user/queue/specific_user";
+
+    public final static String CHAT_IN_COMMON_CHANNEL = "/chat/topic/common";
+
+    public static final String SECURED_CHAT = "/secured/chat";
+
+    public static final String SECURED_CHAT_ROOM = "/secured/room";
 
 }
