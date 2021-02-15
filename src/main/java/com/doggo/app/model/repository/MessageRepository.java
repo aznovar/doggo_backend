d286abd0ad6136591f3ecc5379d4ed96ec733892
aznovar <kkrieger5@gmail.com>
2021-02-15 15:01:12 +0300

    Исправил мелкие недоработки

--- src/main/java/com/doggo/app/model/repository/MessageRepository.java
+++ src/main/java/com/doggo/app/model/repository/MessageRepository.java
@@ -13,7 +13,7 @@ public interface MessageRepository extends JpaRepository<Messages, Long> {
     @Query(value = "select new com.doggo.app.model.dto.GetChatByUserDto(m1.senderId, m1.receiverId, m1.message," +
             "m1.messageDate,m1.messageTypeId) from Messages m1 left join Messages as m2 on m1.senderId = m2.senderId" +
             " and m1.receiverId = m2.receiverId or m1.senderId = m2.receiverId and m1.receiverId = m2.receiverId" +
-            " and  m1.messageId < m2.messageId where  m1.senderId =: userId or m1.receiverId =: userId" +
+            " and  m1.messageId < m2.messageId where  m1.senderId = :userId or m1.receiverId = :userId" +
             " and m2.messageId is null order by m1.messageDate desc ")
     List<GetChatByUserDto> getChatsByUser(@Param("userId") Long userId);
 }
